db4eac8eaa2f5d50e40232becc0c435f8ddaadaa
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-01-28 18:39:12 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='batch_processing_analysis',
-    version='1.0.1',
+    version='1.0.2',
     package_dir={"": "src/batch_processing_analysis"},
     include_package_data=True,
     install_requires=[
