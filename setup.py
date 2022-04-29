52781130a4c457f415850d50d63665abb3fadd34
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-04-29 11:07:49 +0300

    Fixing setup.py to install batch_processing_analysis package with pip-install

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='batch_processing_analysis',
     version='1.1.0',
-    package_dir={"": "src/batch_processing_analysis"},
+    package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         'numpy',
